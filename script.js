7aec16b3749498fca23763c1d15e4876f224f638
nwil850 <nwil850@aucklanduni.ac.nz>
2024-04-13 00:53:06 +1200

    Here is missing script for Javas html

---
+++ script.js
@@ -0,0 +1,11 @@
+var number = 10;
+var string = 'Hello There';
+var isRad = true;
+
+
+if (number == 10) {
+    console.log('Yea buddy');
+} else {
+    console.log('Nope');
+}
+document.getElementById('box').innerHTML = number + 5;
